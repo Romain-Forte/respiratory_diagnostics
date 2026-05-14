59bdf23982b0761977190c9b02b5db66d16d4e99
romain <romain>
2026-05-14 12:40:10 +0200

    rm useless docs

--- R/lecture_dataset.R
+++
@@ -1,8 +0,0 @@
-library(readr)
-
-data <- read_csv("C:/Users/Famiréa/Desktop/EFRAIMstatVDVD.csv",header = TRUE)
-data <- read.csv("C:/Users/Famiréa/Desktop/EFRAIMstatVDVD.csv",header = TRUE)
-
-# Afficher les 6 premières lignes
-head(data)
-
